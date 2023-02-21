42f330746994b8703b3d962047d04603a2bfed59
Werton <mr.werton@gmail.com>
2023-02-21 15:18:29 -0300

    feat: created validator for to valid url

---
+++ atv-scrapping-with-py/urlValidator.py
@@ -0,0 +1,12 @@
+import re
+
+
+def UrlValidator(url: str):
+    regex = re.compile(r'^(?:http|ftp)s?://'
+                       r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+                       r'localhost|'
+                       r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+                       r'(?::\d+)?'
+                       r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    isValid = regex.match(url)
+    return bool(isValid)
