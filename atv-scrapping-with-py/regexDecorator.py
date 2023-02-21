dc888315d8d644052dbc938d77e9c07e22fe0729
Werton <mr.werton@gmail.com>
2023-02-21 15:18:07 -0300

    feat: created decorator add specif regex in re python lib

---
+++ atv-scrapping-with-py/regexDecorator.py
@@ -0,0 +1,34 @@
+import re
+
+
+class RegexDecorator:
+    @staticmethod
+    def getHrefs(document: str):
+        regex = r'href=[\'"]?([^\'" >]+)'
+        result = re.findall(regex, document)
+        if result:
+            return result
+        else:
+            message = 'not found hrefs'
+            return [message]
+
+    @staticmethod
+    def getContentInsideOfTag(tag: str, document: str):
+        regex = r'<{tag}.*?>(.*?)</{tag}>'.format(tag=tag)
+        result = re.findall(regex, document)
+        if result:
+            return result
+        else:
+            message = 'not found result with the tag ' + tag
+            return [message]
+
+    @staticmethod
+    def getTwentyBeforeAndAfterChar(document, word):
+        sensitivePattern = re.compile(word, re.IGNORECASE)
+        result = re.search(sensitivePattern, document)
+        if result:
+            before = document[(result.start() - 10):result.start()]
+            after = document[result.end():result.end() + 10]
+            return [before, after]
+        else:
+            return None
